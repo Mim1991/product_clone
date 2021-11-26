d2be63dfefe75efbda2e693e4387b71603a7ddb4
Meehir Patel <meehir_patel@hotmail.co.uk>
2021-11-26 09:59:53 +0000

    Add simple Github Action workflow

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,4 +4,21 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def new
+    @new_product = Product.new
+  end
+
+  def create
+    @products = Product.new(product_params)
+    if @products.save
+      redirect_to root_path
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :tagline)
+  end
 end
